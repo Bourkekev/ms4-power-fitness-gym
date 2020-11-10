9ca18f19f6623f96d3c494d361f9bf81fc94c0b3
Kevin <bourkekev@gmail.com>
2020-11-10 17:15:57 +0000

    Added admin classes to display fields in admin, and rename categorys in admin

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 from .models import Product, Category
 
-# Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'price',
+        'category',
+        'has_sizes',
+        'image',
+    )
+
+    ordering = ('name',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Product, ProductsAdmin)
+admin.site.register(Category, CategoryAdmin)
