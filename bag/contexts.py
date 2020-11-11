c00707c71d048f44f6e0679cc60e21aa41c58c22
Kevin <bourkekev@gmail.com>
2020-11-11 16:29:00 +0000

    Added delivery variables, note about free delivery and logic in bag contexts

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,5 +1,30 @@
+from decimal import Decimal
+from django.conf import settings
+
+
 def bag_contents(request):
- 
-    context = {}
- 
+
+    bag_items = []
+    total = 0
+    product_count = 0
+
+    if total < settings.FREE_DELIVERY_THRESHOLD:
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+        left_to_free_delivery = settings.FREE_DELIVERY_THRESHOLD - total
+    else:
+        delivery = 0
+        left_to_free_delivery = 0
+
+    grand_total = delivery + total
+
+    context = {
+        'bag_items': bag_items,
+        'total': total,
+        'product_count': product_count,
+        'delivery': delivery,
+        'left_to_free_delivery': left_to_free_delivery,
+        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
+        'grand_total': grand_total,
+    }
+
     return context
