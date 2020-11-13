4c7d25e3a2c8287281f07d2fa5a02d283055ec88
Kevin <bourkekev@gmail.com>
2020-11-13 09:08:52 +0000

    Get bag working for clothes sizes and shoe sizes

--- bag/contexts.py
+++ bag/contexts.py
@@ -20,15 +20,43 @@ def bag_contents(request):
     for item_id, item_data in bag.items():
         if isinstance(item_data, int):
             print('Value is int')
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
         else:
             print('Value is dict')
             # print(item_data['items_by_shoesize'])
             if 'items_by_shoesize' in item_data.keys():
                 print("Shoesize Present, ", end =" ") 
                 print("value =", item_data['items_by_shoesize'])
+                product = get_object_or_404(Product, pk=item_id)
+                for shoesize, quantity in item_data['items_by_shoesize'].items():
+                    total += quantity * product.price
+                    product_count += quantity
+                    bag_items.append({
+                        'item_id': item_id,
+                        'quantity': quantity,
+                        'product': product,
+                        'shoesize': shoesize,
+                    })
             elif 'items_by_clothing_size' in item_data.keys():
                 print("Clothes size Present, ", end =" ") 
                 print("value =", item_data['items_by_clothing_size'])
+                product = get_object_or_404(Product, pk=item_id)
+                for clothing_size, quantity in item_data['items_by_clothing_size'].items():
+                    total += quantity * product.price
+                    product_count += quantity
+                    bag_items.append({
+                        'item_id': item_id,
+                        'quantity': quantity,
+                        'product': product,
+                        'clothing_size': clothing_size,
+                    })
             else: 
                 print("Not present")
         
