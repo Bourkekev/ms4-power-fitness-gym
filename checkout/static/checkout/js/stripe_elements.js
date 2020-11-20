d91de8f0083cfb1ae8e32d2bba0ae41b890d2e23
Kevin <bourkekev@gmail.com>
2020-11-20 19:10:06 +0000

    Added Stripe to checkout page, custom stripe css and stripe js

---
+++ checkout/static/checkout/js/stripe_elements.js
@@ -0,0 +1,32 @@
+/*
+    Core logic/payment flow for this comes from:
+    https://stripe.com/docs/payments/accept-a-payment
+    CSS from: 
+    https://stripe.com/docs/stripe-js
+*/
+
+let stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
+let client_secret = $('#id_client_secret').text().slice(1, -1);
+let stripe = Stripe(stripe_public_key);
+let elements = stripe.elements();
+
+/** Create styles for card */
+let style = {
+    base: {
+        color: "#32325d",
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSmoothing: "antialiased",
+        fontSize: "16px",
+        "::placeholder": {
+            color: "#aab7c4",
+        },
+    },
+    invalid: {
+        color: "#dc3545",
+        iconColor: "#dc3545",
+    },
+};
+// Create card and pass styles
+let card = elements.create('card', {style: style});
+// Mount on div in checkout.html
+card.mount('#card-element');
