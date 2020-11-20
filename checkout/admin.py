b88b54c0c7187b945dd428c1c481f2b0218686cc
Kevin <bourkekev@gmail.com>
2020-11-20 07:32:24 +0000

    Add checkout order classes to allow viewing/editing of orders in admin

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
-# Register your models here.
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    """
+
+    Allows adding and editing of line items in the
+    admin right from inside the order model.
+
+    """
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    """
+
+    Create the admin interface for Orders
+
+    """
+    #  Make OrderLineItem model accessible
+    inlines = (OrderLineItemAdminInline,)
+    readonly_fields = ('order_number', 'date',
+                       'delivery_cost', 'order_total',
+                       'grand_total', )
+    # specify fields order
+    fields = ('order_number', 'date', 'full_name',
+              'email', 'phone_number', 'country',
+              'postcode', 'town_or_city', 'street_address1',
+              'street_address2', 'county', 'delivery_cost',
+              'order_total', 'grand_total',)
+    # displayed columns
+    list_display = ('order_number', 'date', 'full_name',
+                    'order_total', 'delivery_cost',
+                    'grand_total',)
+    # Latest orders at top
+    ordering = ('-date',)
+
+
+admin.site.register(Order, OrderAdmin)
