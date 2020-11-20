1945aad8b939b80b1638678cb2c68e58e359b946
Kevin <bourkekev@gmail.com>
2020-11-20 07:44:36 +0000

    Added signals to update order total on update or delete of line items

--- checkout/apps.py
+++ checkout/apps.py
@@ -3,3 +3,9 @@ from django.apps import AppConfig
 
 class CheckoutConfig(AppConfig):
     name = 'checkout'
+
+    def ready(self):
+        """
+        Update order totals when line item changed/deleted
+        """
+        import checkout.signals
