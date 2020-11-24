b19a15700d99607170f7ec06ea6ae77ef0c19338
Kevin <bourkekev@gmail.com>
2020-11-24 08:08:04 +0000

    Added post handler to profile and adjust toast to not show items in bag

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import UserProfile
 from .forms import EditUserProfile
@@ -7,6 +8,13 @@ from .forms import EditUserProfile
 def profile(request):
     """ Display user's profile """
     profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = EditUserProfile(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+
     form = EditUserProfile(instance=profile)
     orders = profile.orders.all()
 
@@ -14,6 +22,8 @@ def profile(request):
     context = {
         'form': form,
         'orders': orders,
+        'profile_name': profile,
+        'on_profile_page': True
     }
 
     return render(request, template, context)
