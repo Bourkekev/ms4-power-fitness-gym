4561509c64845ea37cbe6c81cb76fffe7eb0295d
Kevin <bourkekev@gmail.com>
2020-11-24 09:18:25 +0000

    Add order history view, url and save users info on checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -7,6 +7,8 @@ from django.conf import settings
 
 from .forms import OrderForm
 from .models import Order, OrderLineItem
+from profiles.forms import EditUserProfile
+from profiles.models import UserProfile
 from products.models import Product
 from bag.contexts import bag_contents
 
@@ -148,6 +150,29 @@ def checkout_success(request, order_number):
     """
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
+
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        # Attach the user's profile to the order
+        order.user_profile = profile
+        order.save()
+
+    # Save the user's info
+    if save_info:
+        profile_data = {
+            'default_email': order.email,
+            'default_phone_number': order.phone_number,
+            'default_postcode': order.postcode,
+            'default_town_or_city': order.town_or_city,
+            'default_street_address1': order.street_address1,
+            'default_street_address2': order.street_address2,
+            'default_county': order.county,
+            'default_country': order.country,
+        }
+        user_profile_form = EditUserProfile(profile_data, instance=profile)
+        if user_profile_form.is_valid():
+            user_profile_form.save()
+
     messages.success(request, f'Your order was successfully processed! \
         Your order number is {order_number}. A confirmation \
         email will be sent to {order.email}.')
