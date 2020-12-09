719a6cb9bf64824c8f4a7fdc7403768ac515a672
Kevin <bourkekev@gmail.com>
2020-12-09 18:12:45 +0000

    Add modal to cancel membership, check sub buttons exist before add event listener

--- config/settings.py
+++ config/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['power-fitness.herokuapp.com', '31fd933863ac.ngrok.io', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['power-fitness.herokuapp.com', 'bddfef303c09.ngrok.io', '127.0.0.1', 'localhost']
 
 
 # Application definition
--- memberships/static/js/stripe-membership.js
+++ memberships/static/js/stripe-membership.js
@@ -8,35 +8,38 @@ fetch("/memberships/config/")
     const stripe = Stripe(data.publicKey);
 
     // Event handler for Gold plan
-    const submitBtnGold = document.querySelector("#submitBtnGold");
-    submitBtnGold.addEventListener("click", () => {
-        price_id = (submitBtnGold.dataset.price_id);
-        // Get Checkout Session ID
-        fetch(`/memberships/create-checkout-session/${price_id}`)
-        .then((result) => { return result.json(); })
-        .then((data) => {
-            console.log(data);
-            // Redirect to Stripe Checkout
-            return stripe.redirectToCheckout({sessionId: data.sessionId})
-        })
-        .then((res) => {
-            console.log(res);
+    if (document.querySelector("#submitBtnGold")) {
+        const submitBtnGold = document.querySelector("#submitBtnGold");
+        console.log(submitBtnGold.length);
+        submitBtnGold.addEventListener("click", () => {
+            price_id = (submitBtnGold.dataset.price_id);
+            // Get Checkout Session ID
+            fetch(`/memberships/create-checkout-session/${price_id}`)
+            .then((result) => { return result.json(); })
+            .then((data) => {
+                console.log(data);
+                // Redirect to Stripe Checkout
+                return stripe.redirectToCheckout({sessionId: data.sessionId})
+            })
+            .then((res) => {
+                console.log(res);
+            });
         });
-    });
-    // Event handler for Platinum plan
-    const submitBtnPlat = document.querySelector("#submitBtnPlat");
-    submitBtnPlat.addEventListener("click", () => {
-        price_id = (submitBtnPlat.dataset.price_id);
-        // Get Checkout Session ID
-        fetch(`/memberships/create-checkout-session/${price_id}`)
-        .then((result) => { return result.json(); })
-        .then((data) => {
-            console.log(data);
-            // Redirect to Stripe Checkout
-            return stripe.redirectToCheckout({sessionId: data.sessionId})
-        })
-        .then((res) => {
-            console.log(res);
+        // Event handler for Platinum plan
+        const submitBtnPlat = document.querySelector("#submitBtnPlat");
+        submitBtnPlat.addEventListener("click", () => {
+            price_id = (submitBtnPlat.dataset.price_id);
+            // Get Checkout Session ID
+            fetch(`/memberships/create-checkout-session/${price_id}`)
+            .then((result) => { return result.json(); })
+            .then((data) => {
+                console.log(data);
+                // Redirect to Stripe Checkout
+                return stripe.redirectToCheckout({sessionId: data.sessionId})
+            })
+            .then((res) => {
+                console.log(res);
+            });
         });
-    });
+    }
 });
