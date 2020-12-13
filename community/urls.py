b5911b760095256c47f2b728faff72dc6be318d6
Kevin <bourkekev@gmail.com>
2020-12-13 11:02:04 +0000

    Added delete message view, url and added confirmation modal to topic template

--- community/urls.py
+++ community/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('topic/<int:topic_id>/reply/', views.reply_post, name='reply_post'),
     path('topic/<int:topic_id>/edit_post/<int:post_id>/',
          views.edit_post, name='edit_post'),
+    path('topic/<int:topic_id>/delete_post/<int:post_id>/',
+         views.delete_post, name='delete_post'),
     path('add_topic', views.add_topic, name='add_topic'),
 ]
