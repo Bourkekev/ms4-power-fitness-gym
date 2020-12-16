c9af2415c086577b763f456b7010d6ebfb8f2813
Kevin <bourkekev@gmail.com>
2020-12-16 07:22:58 +0000

    Change ordering on news posts

--- news/models.py
+++ news/models.py
@@ -20,6 +20,9 @@ class NewsPost(models.Model):
     slug = models.SlugField(max_length=255, unique=True)
     status = models.IntegerField(choices=STATUS, default=0)
 
+    class Meta:
+        ordering = ['-created_on']
+
     def __str__(self):
         return self.title
 
