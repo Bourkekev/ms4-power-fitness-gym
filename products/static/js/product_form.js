ea45b0ab3b44c687672a8ea3d6639842750f66d9
Kevin <bourkekev@gmail.com>
2020-12-20 18:23:10 +0000

    On Add new product, get selected category when selected

--- products/static/js/product_form.js
+++ products/static/js/product_form.js
@@ -2,3 +2,18 @@ $('#new-image').change(function() {
     let file = $('#new-image')[0].files[0];
     $('#filename').text(`Image will be set to: ${file.name}`);
 });
+
+// Hide sizes options depending on category selected
+const hideSizes = function(){
+    //console.log($(this));
+    //console.log($(this).find(":selected").text());
+    if ($(this).find(":selected").text()=="Clothing"){
+        console.log("Clothing selected");
+    }
+    else if ($(this).find(":selected").text()=="Trainers"){
+        console.log("Trainers selected");
+    }
+    else {}
+    
+}
+$('#id_category').change(hideSizes);
