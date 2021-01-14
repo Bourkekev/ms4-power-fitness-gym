301653f3861b5930a1895bdf7e76dd865c49ae15
Kevin <bourkekev@gmail.com>
2021-01-14 17:26:30 +0000

    Added test for new post detail view

--- news/tests.py
+++ news/tests.py
@@ -33,3 +33,14 @@ class NewsPostModelTest(TestCase):
     def test_post_list_view(self):
         response = self.client.get(reverse('news_list'))
         self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Test body text')
+        self.assertTemplateUsed(response, 'news/news_list.html')
+
+    def test_post_detail_view(self):
+        response = self.client.get('/news/1/')
+        no_response = self.client.get('/news/100000/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, 'A test title')
+        self.assertTemplateUsed(response, 'news/news_post_detail.html')
+
