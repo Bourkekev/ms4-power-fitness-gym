43c5561860f2ee020ed718927fd7958e2ff828b1
Kevin <bourkekev@gmail.com>
2021-01-14 17:16:56 +0000

    Added test in news app for confirm news page returns status 200, and test string representation

--- news/tests.py
+++ news/tests.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
+
 from .models import NewsPost
 
 
@@ -18,7 +20,16 @@ class NewsPostModelTest(TestCase):
             author=self.user,
         )
 
-    # Test for setUp NewsPost title
+    def test_string_representation(self):
+        post = NewsPost(title='A test title')
+        self.assertEqual(str(post), post.title)
+
+    # Test for setUp NewsPost content
     def test_post_content(self):
         self.assertEqual(f'{self.post.title}', 'A test title')
         self.assertEqual(f'{self.post.body}', 'Test body text')
+        self.assertEqual(f'{self.post.author}', 'testuser')
+
+    def test_post_list_view(self):
+        response = self.client.get(reverse('news_list'))
+        self.assertEqual(response.status_code, 200)
