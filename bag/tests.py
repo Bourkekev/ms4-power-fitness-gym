8be395db3137e08a51d511ebd26a6dbb5ec22e1e
Kevin <bourkekev@gmail.com>
2021-01-21 08:34:41 +0000

    Added view_bag test

--- bag/tests.py
+++ bag/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class BagViewsTests(TestCase):
+    def test_view_bag_view(self):
+        response = self.client.get(reverse('view_bag'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'bag/bag.html')
