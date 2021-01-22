9e35b4e2c9b784d41b63a54e79379a542203dbcf
Kevin <bourkekev@gmail.com>
2021-01-22 18:08:00 +0000

    Added memberships tests

--- memberships/tests.py
+++ memberships/tests.py
@@ -1,3 +1,24 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class TestMembershipViews(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='testuser',
+            email='test@email.com',
+            password='secret'
+        )
+
+    def test_membership_dashboard_view_logged_in(self):
+        self.client.login(
+            username='testuser',
+            password='secret',
+        )
+        response = self.client.get(reverse('memberships-dashboard'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response,
+                            'active Membership or subscribe to one.')
+        self.assertTemplateUsed(response,
+                                'memberships/memberships-dashboard.html')
